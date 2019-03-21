dc231bd0d33b01a44d228aa34da114b5b31d5637
Sachinbc <48585633+Sachinbc@users.noreply.github.com>
2019-03-21 11:37:47 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,16 +1,2 @@
 print("enter a number")
-num = int(input())
 
-if num > 1:
-    for i in range(2, num):
-        a=(num % i)
-      if a == 0:
-          print(num, "is not a prime number")
-        print(i, "times", int(num/i), "is", num)
-    else:
-        print(num, "is a prime number")
-
-
-
-else:
-    print(, "is not a prime number")
